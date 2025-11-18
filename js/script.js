a68a2718b1245eaee4d721e86f1408da7707f2ec
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-11-18 11:53:05 -0600

    responsive additions and starting mobile styling

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+window.addEventListener("load", function() {
+  let userChoice = this.window.confirm("This window is ment to be viewed on a 1920x1080 monitor. While others will work it wont be the intened viewing experience. Do you wish to continue?");
+  if(userChoice) {
+    console.log("is fine with the constraints");
+  }
+  else {
+    console.log("wishes to switch themes");
+  }
+});
+*/
